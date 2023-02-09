01501760f476c3b74910e8758037f0597d1d2d27
Thavasumoorthi-S <thavasumoorthi.s@tringapps.com>
2023-02-09 11:36:13 +0530

    first commit

--- src/main/java/org/example/Fileopds.java
+++ src/main/java/org/example/Fileopds.java
@@ -1,7 +1,38 @@
 package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class Fileopds
+{
+    Logger logger=Logger.getLogger("com-api-jar");
+    public static void main(String[] args) throws IOException {
+        Logger logger=Logger.getLogger("com-api-jar");
+        String read;
+        int count = 0;
+        HashMap<String, Integer> hmap = new HashMap<String, Integer>();
+        Queue<Map.Entry<String,Integer>> queue=new PriorityQueue<>((a,b)->{return b.getValue()-a.getValue();});
+        FileReader fr = new FileReader("C:\\Users\\Tringapps-User8\\IdeaProjects\\Fileoperation\\src\\main\\java\\org\\example\\student.txt");
+        BufferedReader br = new BufferedReader(fr);
+        while ((read = br.readLine()) != null)
+        {
+            String[] cont = read.split(" ");
+            count = count + cont.length;
+            for (String s : cont) {
+                if (hmap.containsKey(s)) {
+                    hmap.put(s, hmap.get(s) + 1);
+                } else {
+                    hmap.put(s, 1);
+                }
+            }
+        }
+        logger.log(Level.INFO,()->""+hmap.clone());
+        logger.info("Total No of Words in the File"+count);
+        queue.addAll(hmap.entrySet());
+        for(Map.Entry<String, Integer> que:queue) {
+            logger.info(que.getKey() + " =>" + que.getValue() + " \n");
+        }
     }
 }
